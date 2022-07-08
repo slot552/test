29ad890e73b74ca87f62bf4254e29fef1cb3b4e6
slot552 <108933354+slot552@users.noreply.github.com>
2022-07-08 19:14:25 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1", "g", $2)}' >> ips.txt
+num=`cat ips.txt |wc -l`
+s=$(sed -n '1p' ips.txt)
+for ((i=1; i<=num; i ++)); do
+port=$((3000+$i))
+ips=$(sed -n "${i}p" ips.txt)
+echo "internal: $s  port =$port"  >> $port.conf
+echo "external: $ips" >> $port.conf
+echo "socksmethod: username
+user.privileged:root
+user.notprivileged: nobody
+client pass {
+        from: 0.0.0.0/0 port 1-65535 to: 0.0.0.0/0
+}
+socks pass {
+        from: 0.0.0.0/0 port 1-65535 to: 0.0.0.0/0
+        protocol: tcp udp
+}" >> $port.conf
+echo "$ips   $s:$port" >> done.txt
+sockd -f /etc/s5conf/$port.conf &
+done
