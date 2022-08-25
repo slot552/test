0c2879c9b3a19a3d01dd9e6b08a96f234ce9215a
slot552 <108933354+slot552@users.noreply.github.com>
2022-08-25 13:55:14 +0800

    Update rerun.sh

--- rerun.sh
+++ rerun.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 ps -efww|grep "sockd -f" |grep -v grep|cut -c 9-16|xargs kill -9
-num=`cat conf.txt |wc -l`
+num=`cat /etc/s5conf/conf.txt |wc -l`
 for ((i=1; i<=num; i ++)); do
-conf=$(sed -n "${i}p" conf.txt)
-sockd -f /etc/s5conf/$conf &
+conf=$(sed -n "${i}p" /etc/s5conf/conf.txt)
+/usr/local/sbin/sockd -f /etc/s5conf/$conf &
 done
