13c18275c0d76609a475c62899507e49cf2c5c84
slot552 <108933354+slot552@users.noreply.github.com>
2022-12-13 22:30:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ ip addr | awk '/^[0-9]+: / {}; /inet.*global/ {print gensub(/(.*)\/(.*)/, "\\1",
 num=`cat ips.txt |wc -l`
 s=$(sed -n '1p' ips.txt)
 for ((i=1; i<=num; i ++)); do
-port=$((3000+$i))
+port=$((30000+$i))
 ips=$(sed -n "${i}p" ips.txt)
 echo "internal: $s  port =$port"  >> $port.conf
 echo "external: $ips" >> $port.conf
@@ -17,6 +17,6 @@ socks pass {
         from: 0.0.0.0/0 port 1-65535 to: 0.0.0.0/0
         protocol: tcp udp
 }" >> $port.conf
-echo "$ips:$s:$port:" >> done.txt
+echo "$ips-$s:$port:" >> done.txt
 sockd -f /etc/s5conf/$port.conf &
 done
